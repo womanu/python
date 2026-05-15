5d91534975f4a520f6223a04d53ca559891b6dc5
womanu <drumondestevam@gmail.com>
2026-05-15 20:40:26 -0300

    correção

--- exercicio12.py
+++ exercicio12.py
@@ -2,4 +2,4 @@ valor = float(input("Digite o valor de sua compra: "))
 if valor >=100:
     print("Você possui desconto")
 else: 
-    print("Você não possui desconto")
+    print("Você não possui desconto!!!")
